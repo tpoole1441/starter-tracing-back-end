3b45d23ec92e18b05968c16a4d4571e6ce494627
Tyler Poole <tyler.poole1441@gmail.com>
2024-08-27 18:11:12 -0500

    Cleanup Commit

--- src/articles/articles.controller.js
+++ src/articles/articles.controller.js
@@ -31,10 +31,13 @@ async function create(req, res) {
 }
 
 async function list(req, res) {
+  const methodName = "list";
+  req.log.debug({ __filename, methodName });
   const data = await service.list();
   res.json({
     data,
   });
+  req.log.trace({ __filename, methodName, return: true, data });
 }
 
 module.exports = {
